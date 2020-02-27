2003ef9b6248f6987cfa640b31d19acdbc4c1779
alifarah94 <45512716+alifarah94@users.noreply.github.com>
2020-02-27 12:45:43 +0000

    Add files via upload

--- Matlab Lab4/MDtf.m
+++ Matlab Lab4/MDtf.m
@@ -2,14 +2,30 @@ M = 1;
 b = (4);
 k = (5);
 F = 1;
-t = (0:0.1:3);
+t = (0:0.01:3);
 P = tf(F,[M,b,k]);
+Kp = 300; % proportional term
+Ki = 300; % intergral term
+Kd = 50; % derivative term
 
+s = tf('s'); % s
+C = Kp+(Ki/s)+Kd*s; % controller
+Ys = C*P;
+Rs = 1+C*P;
+OL = C*P; % open loop 
+CL = minreal(Ys/Rs); % close loop
 bode(P);
-step(P,t)
+
+
+%step(P,t)
 % the bandwidth of P is 1.68 rad/s 
 % the initial value is 0 and the final sate value is 0.2
 
 % the step responce shows that the final value is 0.2 
 % the rise time is 1.28s
 % the settle time is 2.08s
+
+step(CL,t)
+% the step responce shows that there is off shooting
+% the off shootings causes oscillation which adds more time for the signal
+% to stabilize
